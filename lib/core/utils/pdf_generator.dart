eed3ab88adbe236c0394daa2fc8f4dc69cc0bee2
PorKeat <alexkgm2412@gmail.com>
2025-08-05 18:40:07 +0700

    feat: Implement PDF generation chunks

---
+++ lib/core/utils/pdf_generator.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+import 'package:pdf/pdf.dart';
+import 'package:pdf/widgets.dart' as pw;
+import 'package:path_provider/path_provider.dart';
+
+class PdfGenerator {
+  static Future<File> generatePdfFromImage(File imageFile) async {
+    final pdf = pw.Document();
+    
+    final image = pw.MemoryImage(
+      imageFile.readAsBytesSync(),
+    );
+
+    pdf.addPage(
+      pw.Page(
+        pageFormat: PdfPageFormat.a4,
+        build: (pw.Context context) {
+          return pw.Center(
+            child: pw.Image(image, fit: pw.BoxFit.contain),
+          );
+        },
