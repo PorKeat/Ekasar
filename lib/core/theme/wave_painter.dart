f1078fb9906e5f00e4bc0d46caaa3c7216d8025c
PorKeat <alexkgm2412@gmail.com>
2025-08-17 18:40:07 +0700

    feat: Work on wave animation logic

--- lib/core/theme/wave_painter.dart
+++ lib/core/theme/wave_painter.dart
@@ -9,3 +9,17 @@ class WavePainter extends CustomPainter {
 
   @override
   void paint(Canvas canvas, Size size) {
+    // Draw from back to front
+    _drawWave(
+      canvas, 
+      size, 
+      color: AppColors.waveTertiary.withOpacity(0.4), 
+      offset: math.pi, 
+      heightOffset: 20, 
+      amplitude: 30
+    );
+
+    _drawWave(
+      canvas, 
+      size, 
+      color: AppColors.waveSecondary.withOpacity(0.6), 
