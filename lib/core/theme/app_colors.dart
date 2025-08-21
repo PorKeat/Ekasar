f269db6effe8ef9dd7fb1ce00dc5a0531f331ead
PorKeat <alexkgm2412@gmail.com>
2025-08-21 18:40:07 +0700

    style: Update color palette definitions

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Primary deep purple/blue scheme
+  static const Color primary = Color(0xFF4A3E92);
+  static const Color secondary = Color(0xFFF15A59);
+  
+  // Light Mode
+  static const Color backgroundLight = Color(0xFFF6F8FC);
+  static const Color cardLight = Colors.white;
+  static const Color textLightPrimary = Color(0xFF1E1E2C);
+  static const Color textLightSecondary = Color(0xFF757575);
+
