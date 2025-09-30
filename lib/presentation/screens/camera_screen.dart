ee41cd029288b44586ae632512381bf9cc73cc31
PorKeat <alexkgm2412@gmail.com>
2025-09-30 18:40:07 +0700

    feat: Work on camera viewfinder and controls

---
+++ lib/presentation/screens/camera_screen.dart
@@ -0,0 +1,21 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:pdf_scanner_pro/core/theme/app_colors.dart';
+import 'package:pdf_scanner_pro/presentation/state/camera_provider.dart';
+import 'package:pdf_scanner_pro/presentation/screens/image_preview_screen.dart';
+import 'package:pdf_scanner_pro/presentation/widgets/custom_snackbar.dart';
+
+class CameraScreen extends ConsumerWidget {
+  const CameraScreen({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final cameraState = ref.watch(cameraControllerProvider);
+
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: SafeArea(
+        child: cameraState.when(
+          data: (controller) {
