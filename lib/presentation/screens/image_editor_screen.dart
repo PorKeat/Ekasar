aadeb3ab752704cf7906979c1125e4b53a290b7f
PorKeat <alexkgm2412@gmail.com>
2025-10-08 18:40:07 +0700

    feat: Implement image editor filters and crop

---
+++ lib/presentation/screens/image_editor_screen.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image/image.dart' as img;
+import 'package:pdf_scanner_pro/core/theme/app_colors.dart';
+import 'package:pdf_scanner_pro/presentation/screens/pdf_preview_screen.dart';
+import 'package:pdf_scanner_pro/core/utils/pdf_generator.dart';
+import 'package:pdf_scanner_pro/presentation/widgets/custom_snackbar.dart';
+
+enum FilterType { original, magicColor, bwDocument, grayscale, lighten }
+
+class ImageEditorScreen extends StatefulWidget {
+  final File imageFile;
+
+  const ImageEditorScreen({super.key, required this.imageFile});
+
+  @override
+  State<ImageEditorScreen> createState() => _ImageEditorScreenState();
+}
+
+class _ImageEditorScreenState extends State<ImageEditorScreen> {
