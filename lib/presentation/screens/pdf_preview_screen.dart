d3c6b0ef6b5eb404acc8d11a21af8d7520619f1f
PorKeat <alexkgm2412@gmail.com>
2025-11-21 18:40:07 +0700

    feat: Work on PDF preview and share sheets

--- lib/presentation/screens/pdf_preview_screen.dart
+++ lib/presentation/screens/pdf_preview_screen.dart
@@ -8,3 +8,22 @@ import 'package:pdf_scanner_pro/presentation/widgets/custom_snackbar.dart';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:pdf_scanner_pro/data/repositories/document_repository.dart';
+import 'package:pdf_scanner_pro/domain/models/document.dart';
+import 'package:pdf_scanner_pro/presentation/state/settings_provider.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:path/path.dart' as path;
+
+class PdfPreviewScreen extends ConsumerStatefulWidget {
+  final File pdfFile;
+  final List<String> images;
+
+  const PdfPreviewScreen({
+    super.key, 
+    required this.pdfFile,
+    this.images = const [],
+  });
+
+  @override
+  ConsumerState<PdfPreviewScreen> createState() => _PdfPreviewScreenState();
+}
+
