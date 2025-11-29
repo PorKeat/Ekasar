2e5c8d74edbbfffd8c28bdb651732506e1ed08bf
PorKeat <alexkgm2412@gmail.com>
2025-11-29 18:40:07 +0700

    feat: Improve custom snackbar UI

---
+++ lib/presentation/widgets/custom_snackbar.dart
@@ -0,0 +1,12 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:pdf_scanner_pro/core/theme/app_colors.dart';
+
+enum SnackBarType { success, error, warning }
+
+class CustomSnackBar {
+  static void show(
+    BuildContext context, {
+    required String title,
+    required String message,
