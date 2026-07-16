ce1a8b752cc09db71b3ddf29bacba99d1cb20641
PorKeat <alexkgm2412@gmail.com>
2026-07-16 22:00:12 +0700

    fix: Inject missing namespace for older flutter plugins to fix AGP 8+ builds

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,8 +15,24 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+}
+
+subprojects {
+    project.extensions.findByName("android")?.let { android ->
+        try {
+            val getNamespace = android.javaClass.getMethod("getNamespace")
+            val currentNamespace = getNamespace.invoke(android)
+            if (currentNamespace == null) {
+                val setNamespace = android.javaClass.getMethod("setNamespace", String::class.java)
+                setNamespace.invoke(android, project.group.toString())
+            }
+        } catch (e: Exception) {
+            // Ignore if method doesn't exist
+        }
+    }
 }
 
 tasks.register<Delete>("clean") {
